7996186d0d1872f57d390c317052c6fa157c30c6
Flavien Normand <contact@flavien-normand.fr>
2022-05-04 08:15:34 +0200

    feat(tasks): new bulk tracking buttons

--- apps/client/src/app/pages/tasks/tasks/tasks.component.ts
+++ apps/client/src/app/pages/tasks/tasks/tasks.component.ts
@@ -1,15 +1,15 @@
-import {Component} from '@angular/core';
-import {TasksService} from "../tasks.service";
-import {LostarkTask} from "../../../model/lostark-task";
-import {TaskFrequency} from "../../../model/task-frequency";
-import {TaskScope} from "../../../model/task-scope";
-import {FormBuilder, Validators} from "@angular/forms";
-import {NzMessageService} from "ng-zorro-antd/message";
+import { Component } from "@angular/core";
+import { TasksService } from "../tasks.service";
+import { LostarkTask } from "../../../model/lostark-task";
+import { TaskFrequency } from "../../../model/task-frequency";
+import { TaskScope } from "../../../model/task-scope";
+import { FormBuilder, Validators } from "@angular/forms";
+import { NzMessageService } from "ng-zorro-antd/message";
 
 @Component({
-  selector: 'lostark-helper-tasks',
-  templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.less'],
+  selector: "lostark-helper-tasks",
+  templateUrl: "./tasks.component.html",
+  styleUrls: ["./tasks.component.less"]
 })
 export class TasksComponent {
   public TaskFrequency = TaskFrequency;
@@ -18,12 +18,12 @@ export class TasksComponent {
   public tasks$ = this.tasksService.tasks$;
 
   public form = this.fb.group({
-    label: ['', Validators.required],
+    label: ["", Validators.required],
     frequency: [TaskFrequency.DAILY],
     scope: [TaskScope.CHARACTER],
     amount: [null, Validators.required],
     minIlvl: [null, [Validators.required, Validators.min(0), Validators.max(9999)]],
-    maxIlvl: [null, [Validators.required, Validators.min(0), Validators.max(9999)]],
+    maxIlvl: [null, [Validators.required, Validators.min(0), Validators.max(9999)]]
   });
 
   constructor(private tasksService: TasksService,
@@ -43,16 +43,20 @@ export class TasksComponent {
       formData.iconPath,
       true,
       true
-    )
+    );
     this.tasksService.addTask(task);
     this.form.reset({
       frequency: TaskFrequency.DAILY,
       scope: TaskScope.CHARACTER
     });
-    this.message.success('Custom task added to the list');
+    this.message.success("Custom task added to the list");
   }
 
-  updateTask(task: LostarkTask):void{
+  setTrackAll(track: boolean): void {
+    this.tasksService.setTrackAll(track);
+  }
+
+  updateTask(task: LostarkTask): void {
     this.tasksService.updateTask(task);
   }
 
