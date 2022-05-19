35f4b16e12f1decbf5c0f261c1a44ca443232bd7
Flavien Normand <contact@flavien-normand.fr>
2022-05-19 17:54:48 +0200

    chore(tasks): add better banners for guardians

--- apps/client/src/app/pages/party-planner/subtasks.ts
+++ apps/client/src/app/pages/party-planner/subtasks.ts
@@ -2,29 +2,29 @@ import { Subtask } from "./subtask";
 
 export const subtasks: Subtask[] = [
   // T1 Guardians
-  { name: `Ur'nil`, parentName: "Guardian", banner: "guardians/guardian_23.png", minIlvl: 302 },
-  { name: `Lumerus`, parentName: "Guardian", banner: "guardians/guardian_22.png", minIlvl: 340 },
-  { name: `Icy Legoros`, parentName: "Guardian", banner: "guardians/guardian_11.png", minIlvl: 380 },
-  { name: `Vertus`, parentName: "Guardian", banner: "guardians/guardian_00.png", minIlvl: 420 },
-  { name: `Chromanium`, parentName: "Guardian", banner: "guardians/guardian_02.png", minIlvl: 460 },
-  { name: `Nacrasena`, parentName: "Guardian", banner: "guardians/guardian_07.png", minIlvl: 500 },
-  { name: `Flame Fox Yoho`, parentName: "Guardian", banner: "guardians/guardian_19.png", minIlvl: 540 },
-  { name: `Tytalos`, parentName: "Guardian", banner: "guardians/guardian_21.png", minIlvl: 580 },
+  { name: `Ur'nil`, parentName: "Guardian", banner: "guardians/monster_info_23.png", minIlvl: 302 },
+  { name: `Lumerus`, parentName: "Guardian", banner: "guardians/monster_info_22.png", minIlvl: 340 },
+  { name: `Icy Legoros`, parentName: "Guardian", banner: "guardians/monster_info_11.png", minIlvl: 380 },
+  { name: `Vertus`, parentName: "Guardian", banner: "guardians/monster_info_00.png", minIlvl: 420 },
+  { name: `Chromanium`, parentName: "Guardian", banner: "guardians/monster_info_02.png", minIlvl: 460 },
+  { name: `Nacrasena`, parentName: "Guardian", banner: "guardians/monster_info_07.png", minIlvl: 500 },
+  { name: `Flame Fox Yoho`, parentName: "Guardian", banner: "guardians/monster_info_19.png", minIlvl: 540 },
+  { name: `Tytalos`, parentName: "Guardian", banner: "guardians/monster_info_21.png", minIlvl: 580 },
   // T2 Guardians
-  { name: `Dark Legoros`, parentName: "Guardian", banner: "guardians/guardian_10.png", minIlvl: 802 },
-  { name: `Helgaia`, parentName: "Guardian", banner: "guardians/guardian_01.png", minIlvl: 840 },
-  { name: `Calventus`, parentName: "Guardian", banner: "guardians/guardian_17.png", minIlvl: 880 },
-  { name: `Achates`, parentName: "Guardian", banner: "guardians/guardian_20.png", minIlvl: 920 },
-  { name: `Frost Helgaia`, parentName: "Guardian", banner: "guardians/guardian_14.png", minIlvl: 960 },
-  { name: `Lava Chromanium`, parentName: "Guardian", banner: "guardians/guardian_15.png", minIlvl: 1000 },
-  { name: `Levanos`, parentName: "Guardian", banner: "guardians/guardian_09.png", minIlvl: 1040 },
-  { name: `Alberhastic`, parentName: "Guardian", banner: "guardians/guardian_28.png", minIlvl: 1080 },
+  { name: `Dark Legoros`, parentName: "Guardian", banner: "guardians/monster_info_10.png", minIlvl: 802 },
+  { name: `Helgaia`, parentName: "Guardian", banner: "guardians/monster_info_01.png", minIlvl: 840 },
+  { name: `Calventus`, parentName: "Guardian", banner: "guardians/monster_info_17.png", minIlvl: 880 },
+  { name: `Achates`, parentName: "Guardian", banner: "guardians/monster_info_20.png", minIlvl: 920 },
+  { name: `Frost Helgaia`, parentName: "Guardian", banner: "guardians/monster_info_14.png", minIlvl: 960 },
+  { name: `Lava Chromanium`, parentName: "Guardian", banner: "guardians/monster_info_15.png", minIlvl: 1000 },
+  { name: `Levanos`, parentName: "Guardian", banner: "guardians/monster_info_09.png", minIlvl: 1040 },
+  { name: `Alberhastic`, parentName: "Guardian", banner: "guardians/monster_info_28.png", minIlvl: 1080 },
   // T3 Guardians
-  { name: `Armored Nacrasena`, parentName: "Guardian", banner: "guardians/guardian_08.png", minIlvl: 1302 },
-  { name: `Igrexion`, parentName: "Guardian", banner: "guardians/guardian_26.png", minIlvl: 1340 },
-  { name: `Night Fox Yoho`, parentName: "Guardian", banner: "guardians/guardian_18.png", minIlvl: 1370 },
-  // { name: `Velganos`, parentName: "Guardian", banner: 'guardians/guardian_.png', minIlvl: 1400 },
-  { name: `Deskaluda`, parentName: "Guardian", banner: 'guardians/guardian_29.png', minIlvl: 1415 },
+  { name: `Armored Nacrasena`, parentName: "Guardian", banner: "guardians/monster_info_08.png", minIlvl: 1302 },
+  { name: `Igrexion`, parentName: "Guardian", banner: "guardians/monster_info_26.png", minIlvl: 1340 },
+  { name: `Night Fox Yoho`, parentName: "Guardian", banner: "guardians/monster_info_18.png", minIlvl: 1370 },
+  // { name: `Velganos`, parentName: "Guardian", banner: 'guardians/monster_info_.png', minIlvl: 1400 },
+  { name: `Deskaluda`, parentName: "Guardian", banner: 'guardians/monster_info_27.png', minIlvl: 1415 },
 
   // T3 Abyss
   { name: `Aira's Oculus (Normal)`, parentName: "Aira's Oculus", banner: "abyss_dungeons/abyss_dg_09.png", minIlvl: 1325 },
@@ -38,6 +38,6 @@ export const subtasks: Subtask[] = [
   { name: `Argos P3`, parentName: "Argos", banner: "abyss_raids/abyss_02.png", minIlvl: 1400 },
 
   // Valtan Difficulties
-  { name: `Valtan Normal`, parentName: "Valtan", banner: "abyss_raids/abyss_02.png", minIlvl: 1415 },
-  { name: `Valtan Hard`, parentName: "Valtan", banner: "abyss_raids/abyss_02.png", minIlvl: 1445 }
+  { name: `Valtan Normal`, parentName: "Valtan", minIlvl: 1415 },
+  { name: `Valtan Hard`, parentName: "Valtan", minIlvl: 1445 }
 ];
