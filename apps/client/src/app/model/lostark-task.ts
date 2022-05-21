ab2555e271cbf066fb7ccfb560aaee75a1dfd1c4
Flavien Normand <contact@flavien-normand.fr>
2022-05-21 11:55:01 +0200

    chore: tasks version update

--- apps/client/src/app/model/lostark-task.ts
+++ apps/client/src/app/model/lostark-task.ts
@@ -3,7 +3,7 @@ import { TaskScope } from "./task-scope";
 import { DataModel } from "../core/database/data-model";
 
 
-export const TASKS_VERSION = 27;
+export const TASKS_VERSION = 28;
 
 export interface LostarkTask extends DataModel {
   authorId?: string;
