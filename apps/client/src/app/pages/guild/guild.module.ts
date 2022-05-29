f2223bf3b022dab2fd3bff5a2c9813df6f9460dc
Flavien Normand <contact@flavien-normand.fr>
2022-05-29 21:31:50 +0200

    chore: starting planning stuff, design needed

--- apps/client/src/app/pages/guild/guild.module.ts
+++ apps/client/src/app/pages/guild/guild.module.ts
@@ -1,45 +1,52 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GuildHomeComponent } from './guild-home/guild-home.component';
-import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-import { NzListModule } from 'ng-zorro-antd/list';
-import { RouterModule, Routes } from '@angular/router';
-import { NzEmptyModule } from 'ng-zorro-antd/empty';
-import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzFormModule } from 'ng-zorro-antd/form';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { GuildHomeComponent } from "./guild-home/guild-home.component";
+import { NzPageHeaderModule } from "ng-zorro-antd/page-header";
+import { NzListModule } from "ng-zorro-antd/list";
+import { RouterModule, Routes } from "@angular/router";
+import { NzEmptyModule } from "ng-zorro-antd/empty";
+import { NzCardModule } from "ng-zorro-antd/card";
+import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzFormModule } from "ng-zorro-antd/form";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzStatisticModule } from 'ng-zorro-antd/statistic';
-import { NzTagModule } from 'ng-zorro-antd/tag';
-import { CharacterReferenceModule } from '../../components/character-reference/character-reference.module';
-import { IconsProviderModule } from '../../icons-provider.module';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
-import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { GuildDetailsComponent } from './guild-details/guild-details.component';
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzSelectModule } from "ng-zorro-antd/select";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { NzStatisticModule } from "ng-zorro-antd/statistic";
+import { NzTagModule } from "ng-zorro-antd/tag";
+import { CharacterReferenceModule } from "../../components/character-reference/character-reference.module";
+import { IconsProviderModule } from "../../icons-provider.module";
+import { NzTypographyModule } from "ng-zorro-antd/typography";
+import { NzToolTipModule } from "ng-zorro-antd/tooltip";
+import { NzPopconfirmModule } from "ng-zorro-antd/popconfirm";
+import { ClipboardModule } from "@angular/cdk/clipboard";
+import { GuildDetailsComponent } from "./guild-details/guild-details.component";
 import { NzBadgeModule } from "ng-zorro-antd/badge";
 import { NzDividerModule } from "ng-zorro-antd/divider";
 import { PipesModule } from "../../core/pipes/pipes.module";
 import { NzAlertModule } from "ng-zorro-antd/alert";
 import { NzSwitchModule } from "ng-zorro-antd/switch";
+import { GuildPlanningComponent } from "./guild-planning/guild-planning.component";
+import { NzTableModule } from "ng-zorro-antd/table";
 
 const routes: Routes = [
   {
-    path: '',
-    component: GuildHomeComponent,
+    path: "",
+    component: GuildHomeComponent
   },
   {
-    path: ':guildId',
-    component: GuildDetailsComponent,
+    path: ":guildId",
+    component: GuildDetailsComponent
   },
+  {
+    path: ":guildId/planning",
+    component: GuildPlanningComponent
+    // TODO add leader/officier guard
+  }
 ];
 
 @NgModule({
-  declarations: [GuildHomeComponent, GuildDetailsComponent],
+  declarations: [GuildHomeComponent, GuildDetailsComponent, GuildPlanningComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
@@ -67,7 +74,9 @@ const routes: Routes = [
     PipesModule,
     NzAlertModule,
     NzSwitchModule,
-    FormsModule
+    FormsModule,
+    NzTableModule
   ]
 })
-export class GuildModule {}
+export class GuildModule {
+}
--- apps/client/src/app/pages/party-planner/subtask.ts
+++ apps/client/src/app/pages/party-planner/subtask.ts
@@ -1,4 +1,5 @@
 export interface Subtask {
+  id: string;
   name: string;
   parentName: string;
   minIlvl: number;
