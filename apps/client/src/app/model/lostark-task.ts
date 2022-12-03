10eb34ef62088384cbcb8fd67462275d26bc3383
SylightDP <48878664+SylightDP@users.noreply.github.com>
2022-12-03 15:34:40 +0000

    Fix for previous PR by SylightDP

--- apps/client/src/app/model/lostark-task.ts
+++ apps/client/src/app/model/lostark-task.ts
@@ -2,7 +2,7 @@ import { TaskFrequency } from "./task-frequency";
 import { TaskScope } from "./task-scope";
 import { DataModel } from "../core/database/data-model";
 
-export const TASKS_VERSION = 34;
+export const TASKS_VERSION = 35;
 
 export interface LostarkTask extends DataModel {
   authorId?: string;
